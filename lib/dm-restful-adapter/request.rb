7a5bb1cad07fec9de0937bb14cd4a022d120bf78
Hubert Huang <hhuang@optimiscorp.com>
2012-04-11 16:31:39 -0700

    Convert Restful::Request to module

--- lib/dm-restful-adapter/request.rb
+++ lib/dm-restful-adapter/request.rb
@@ -1,24 +1,24 @@
 module Restful
-  class Request
-    def self.post(model_name, attrs)
+  module Request
+    extend self
+
+    def post(model_name, attrs)
       Configuration.backend.call(:post, resourceify(model_name), attrs)
     end
 
-    def self.get(model_name, attrs)
+    def get(model_name, attrs)
       Configuration.backend.call(:get, resourceify(model_name), attrs)
     end
 
-    def self.put(model_name, id, attrs)
+    def put(model_name, id, attrs)
       Configuration.backend.call(:put, resourceify(model_name, id), model_name.downcase => attrs)
     end
 
-    def self.delete(model_name, id)
+    def delete(model_name, id)
       Configuration.backend.call(:delete, resourceify(model_name, id))
     end
 
-    private
-
-    def self.resourceify(model_name, id=nil)
+    def resourceify(model_name, id=nil)
       [Configuration.domain, model_name.downcase.pluralize, id].compact.join('/')
     end
   end
